524880ffb23621869cdd7b2b5711c6a6f9acc9a0
nehls01 <78539918+nehls01@users.noreply.github.com>
2026-02-06 19:30:08 -0600

    Create startup.sh script for automating server startup

---
+++ startup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Automate server startup
+
+# Set date and time
+current_date="2026-02-07 01:29:17"
+echo "Starting server at: $current_date"
+
+# Command to start the server
+# Replace this with the actual command you use to start your server
+# server_start_command
+
+echo "Server has started!"
